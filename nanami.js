4a4f6fba33a8652b2644b0e0e9e81ab54c671b4d
James Tracy <jtracy.emmet@gmail.com>
2020-03-27 19:30:08 -0400

    fixed description

--- nanami.js
+++ nanami.js
@@ -9,7 +9,7 @@ const client = new Discord.Client();
 new AlterEgo(client)
   .setPrefix('n.')
   .setName('Nanami Bot')
-  .setDescription('I let you quicky search for images.')
+  .setDescription('I keep track of your emojis!')
   .setColor('#EBC8CB')
   .setOwnerId('265902301443653644')
   .setInvite('https://discordapp.com/oauth2/authorize?client_id=624386401735147531&permissions=1074129984&scope=bot')
