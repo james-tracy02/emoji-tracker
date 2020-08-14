6bf5cd3be064e892f18a5132abcb89169bf1f858
jtracy02 <jtracy.emmet@gmail.com>
2020-08-14 02:43:31 -0400

    connection pools 2.0

--- src/database.js
+++ src/database.js
@@ -1,20 +1,16 @@
 const mysql = require('mysql');
 const pool = mysql.createPool(process.env.JAWSDB_URL);
 
+/*
 setInterval(ping, 3600000);
 
 function ping() {
-  pool.getConnection((err, connection) => {
+  pool.query('SELECT 1;', (err) => {
     if(err) {
       console.log(err);
-      return;
     }
-    connection.query('SELECT 1;', (err) => {
-      if(err) {
-        console.log(err);
-      }
-    });
   });
 }
+*/
 
 module.exports = pool;
--- src/service/records.js
+++ src/service/records.js
@@ -2,78 +2,54 @@ const pool = require('../database');
 const TABLE_NAME = 'records';
 
 function getRecordsForUser(userId, time, callback) {
-  pool.getConnection((err, connection) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    connection.query(`SELECT * FROM ${TABLE_NAME} WHERE userId = ${userId} AND time >= ?`, [time],
-      (err, result) => {
-        if(err) {
-          console.log("Failed to retrieve emoji data.");
-          console.log(err);
-        } else {
-          console.log('Retrieved records for user.')
-          callback(result);
-        }
-      });
-  });
+  pool.query(`SELECT * FROM ${TABLE_NAME} WHERE userId = ${userId} AND time >= ?`, [time],
+    (err, result) => {
+      if(err) {
+        console.log("Failed to retrieve emoji data.");
+        console.log(err);
+      } else {
+        console.log('Retrieved records for user.')
+        callback(result);
+      }
+    });
 }
 
 function getRecordsForUsers(userIds, time, callback) {
-  pool.getConnection((err, connection) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    connection.query(`SELECT * FROM ${TABLE_NAME} WHERE userId IN (${userIds}) AND time >= ?`, [time],
-      (err, result) => {
-        if(err) {
-          console.log("Failed to retrieve emoji data.");
-          console.log(err);
-        } else {
-          console.log('Retrieved records for users.')
-          callback(result);
-        }
-      });
+  pool.query(`SELECT * FROM ${TABLE_NAME} WHERE userId IN (${userIds}) AND time >= ?`, [time],
+    (err, result) => {
+      if(err) {
+        console.log("Failed to retrieve emoji data.");
+        console.log(err);
+      } else {
+        console.log('Retrieved records for users.')
+        callback(result);
+      }
     });
 }
 
 function getAllRecords(time, callback) {
-  pool.getConnection((err, connection) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    connection.query(`SELECT * FROM ${TABLE_NAME} WHERE time >= ?`, [time],
-      (err, result) => {
-        if(err) {
-          console.log("Failed to retrieve emoji data.");
-          console.log(err);
-        } else {
-          console.log('Retrieved records for all users.')
-          callback(result);
-        }
-      });
+  pool.query(`SELECT * FROM ${TABLE_NAME} WHERE time >= ?`, [time],
+    (err, result) => {
+      if(err) {
+        console.log("Failed to retrieve emoji data.");
+        console.log(err);
+      } else {
+        console.log('Retrieved records for all users.')
+        callback(result);
+      }
     });
 }
 
 function getRecordsForEmoji(emojiId, time, callback) {
-  pool.getConnection((err, connection) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    connection.query(`SELECT * FROM ${TABLE_NAME} WHERE emojiId = ${emojiId} AND time >= ?`, [time],
-      (err, result) => {
-        if(err) {
-          console.log("Failed to retrieve user data.");
-          console.log(err);
-        } else {
-          console.log('Retrieved records for emoji.')
-          callback(result);
-        }
-      });
+  pool.query(`SELECT * FROM ${TABLE_NAME} WHERE emojiId = ${emojiId} AND time >= ?`, [time],
+    (err, result) => {
+      if(err) {
+        console.log("Failed to retrieve user data.");
+        console.log(err);
+      } else {
+        console.log('Retrieved records for emoji.')
+        callback(result);
+      }
     });
 }
 
@@ -82,18 +58,12 @@ function insertRecords(emojiIds, userId, time) {
   emojiIds.forEach(emojiId => {
     values.push([userId, emojiId, time]);
   });
-  pool.getConnection((err, connection) => {
-    if(err) {
-      console.log(err);
-      return;
-    }
-    connection.query(`INSERT INTO ${TABLE_NAME} VALUES ?`, [values],
-        err => {
-          if(err) {
-            console.log("Failed to insert emoji data.");
-            console.log(err);
-          }
-      });
+  pool.query(`INSERT INTO ${TABLE_NAME} VALUES ?`, [values],
+      err => {
+        if(err) {
+          console.log("Failed to insert emoji data.");
+          console.log(err);
+        }
     });
 }
 
