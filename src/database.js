6efbdec42136b05fc41edc93af0025281ccf6961
jtracy02 <jtracy.emmet@gmail.com>
2020-08-26 22:51:23 -0400

    pls

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const pool = mysql.createPool(process.env.JAWSDB_URL);
 pool.connectionLimit = 10;
-pool.connectTimeout = 60 * 1000;
+pool.acquireTimeout = 60 * 1000;
 
 module.exports = pool;
