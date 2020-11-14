0b0c5771c096e6da7e40c1f3f281e897f3deac95
jtracy02 <jtracy.emmet@gmail.com>
2020-11-14 01:08:29 -0500

    reactionTimeout to 180s

--- src/configs.js
+++ src/configs.js
@@ -2,7 +2,7 @@ module.exports = {
   resultsPerPage: 10,
   displayWidth: 60,
   responseLimit: 1800,
-  reactionTimeout: 60000,
+  reactionTimeout: 180000,
   color: "#ebc8cb",
   ownerId: 265902301443653644,
   inviteLink: "https://discord.com/api/oauth2/authorize?client_id=624386401735147531&permissions=1074261057&scope=bot",
